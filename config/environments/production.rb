82306ca79c064883bb5629962385a2ddceb07c63
khoa nguyen <huukhoanguyen@hotmail.com>
2013-07-23 02:16:22 +1000

    send log messages to STDOUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ MemcacheStoreErrorApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 end
